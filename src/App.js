26ba249623e38deda041ae97b89f53ead4ff65f2
Liliya <liliyavoloshine@gmail.com>
2021-07-20 12:02:25 +0400

    feat: add localstorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './css/style.css'
-import data from './data.json'
+// import data from './data.json'
 import GroceryList from './GroceryList'
 import GrocerySorting from './GrocerySorting'
 import GroceryFiltering from './GroceryFiltering'
@@ -10,25 +10,47 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      items: [],
       sortByAmount: false,
       filterText: ''
     }
   }
+
   handleStateSortByAmount(sort) {
     this.setState({
       sortByAmount: sort
     })
   }
+
   handleStateFilterText(text) {
     this.setState({
       filterText: text
     })
   }
 
+  handleStateItems() {
+    const data = JSON.parse(localStorage.getItem('grocery-items'))
+    this.setState({
+      items: data
+    })
+  }
+
+  handleStateAddNewItem(newItem) {
+    const updatedState = this.state.items.concat(newItem)
+    localStorage.setItem('grocery-items', JSON.stringify(updatedState))
+    this.setState({
+      items: updatedState
+    })
+  }
+
+  componentDidMount() {
+    this.handleStateItems()
+  }
+
   render() {
     return (
       <div className="app">
-        <GroceryNew />
+        <GroceryNew itemsLength={this.state.items.length} onAddNewItem={newItem => this.handleStateAddNewItem(newItem)} />
         <div className="grocery-navbar">
           <GroceryFiltering
             onFilterText={text => this.handleStateFilterText(text)}
@@ -39,7 +61,7 @@ class App extends React.Component {
           />
         </div>
         <GroceryList
-          products={data}
+          products={this.state.items}
           sortByPriority={this.state.sortByPriority}
           sortByAmount={this.state.sortByAmount}
           filterText={this.state.filterText}
--- src/GroceryNew.js
+++ src/GroceryNew.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 
 export class GroceryNew extends Component {
-  onSubmit() {
-    console.log('work')
+  constructor(props) {
+    super(props)
+    this.state = {
+      newItem: ''
+    }
   }
+  
+  handleNewItem(e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.onAddNewItem({
+      id: this.props.itemsLength + 1,
+      name: this.state.newItem,
+      amount: 1,
+      bougth: false
+    })
+  }
+
   render() {
     return (
-      <form onSubmit={() => this.onSubmit()} className="grocery-navbar">
+      <form onSubmit={e => this.onSubmit(e)} className="grocery-navbar">
         <div className="grocery-navbar__input">
-          <input placeholder="Add new item..." />
+          <input
+            onChange={e => this.handleNewItem(e)}
+            value={this.state.newItem}
+            placeholder="Add new item..."
+          />
         </div>
         <div className="grocery-navbar__btn">
-          <button>
-            <i class="fas fa-plus"></i>
+          <button type="submit">
+            <i className="fas fa-plus"></i>
           </button>
         </div>
       </form>
